d47f0f95453ccee32d297df598bab5842389a1a1
Igor Sorokopud <igorztsorokopud@gmail.com>
2016-11-07 22:14:00 +0200

    add clock

--- components/index.js
+++ components/index.js
@@ -1,3 +1,43 @@
-/**
- * Created by igor on 11/7/16.
- */
+function Clock(options) {
+    var elem = options.elem;
+    var time;
+    var spanHours = elem.querySelector(".hour");
+    var spanMin = elem.querySelector(".min");
+    var spanSec = elem.querySelector(".sec");
+
+    function render() {
+        var time = new Date();
+
+        var hours = time.getHours();
+        if (hours < 10) {
+            hours = "0" + hours;
+        }
+        spanHours.innerHTML = hours;
+
+        var min = time.getMinutes();
+        if (min < 10) {
+            min = "0" + min;
+        }
+        spanMin.innerHTML = min;
+
+        var sec = time.getSeconds();
+        if (sec < 10) {
+            sec = "0" + sec;
+        }
+        spanSec.innerHTML = sec;
+
+    }
+
+    function start() {
+        stop();
+        render();
+        time = setInterval(render, 1000)
+    }
+
+    function stop() {
+        clearInterval(time);
+    }
+
+    this.start = start;
+    this.stop = stop;
+}
